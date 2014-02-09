32fa048d03efd8a7c3212d04456c517c08633e8b
erw13n <erwin.y@superexpert.com>
2014-02-10 05:57:45 +0700

    First Commit

---
+++ WhaleTeam-TinyMCE/TinyMCEHtmlEditorProvider.cs
@@ -0,0 +1,156 @@
+﻿
+namespace WhaleTeam.TinyMCE
+{
+    #region
+
+    using System.Collections;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+
+    using DotNetNuke.Common;
+    using DotNetNuke.Modules.HTMLEditorProvider;
+
+    using WhaleTeam.TinyMCE.Web;
+
+    #endregion
+
+    public class TinyMCEHtmlEditorProvider: HtmlEditorProvider
+    {
+
+        #region "Private Members"
+
+        /// <summary>
+        /// The _additional toolbars.
+        /// </summary>
+        private ArrayList additionalToolbars = new ArrayList();
+
+        /// <summary>
+        /// The _html editor control.
+        /// </summary>
+        private Web.Textarea htmlEditorControl;
+
+        /// <summary>
+        /// The _root image directory.
+        /// </summary>
+        private string rootImageDirectory;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets AdditionalToolbars.
+        /// </summary>
+        public override ArrayList AdditionalToolbars
+        {
+            get
+            {
+                return this.additionalToolbars;
+            }
+            set
+            {
+                this.additionalToolbars = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets ControlID.
+        /// </summary>
+        public override string ControlID { get; set; }
+
+        /// <summary>
+        /// Gets or sets Height.
+        /// </summary>
+        public override Unit Height
+        {
+            get
+            {
+                return this.htmlEditorControl.Height;
+            }
+            set
+            {
+                this.htmlEditorControl.Height = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets HtmlEditorControl.
+        /// </summary>
+        public override Control HtmlEditorControl
+        {
+            get { return this.htmlEditorControl; }
+        }
+
+        /// <summary>
+        /// Gets or sets RootImageDirectory.
+        /// </summary>
+        public override string RootImageDirectory
+        {
+            get
+            {
+                if (this.rootImageDirectory == string.Empty)
+                {
+                    // Remove the Application Path from the Home Directory
+                    return Globals.ApplicationPath != string.Empty
+                               ? this.PortalSettings.HomeDirectory.Replace(Globals.ApplicationPath, string.Empty)
+                               : this.PortalSettings.HomeDirectory;
+                }
+
+                return this.rootImageDirectory;
+            }
+            set
+            {
+                this.rootImageDirectory = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets Text.
+        /// </summary>
+        public override string Text
+        {
+            get
+            {
+                return this.htmlEditorControl.Value;
+            }
+            set
+            {
+                this.htmlEditorControl.Value = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets Width.
+        /// </summary>
+        public override Unit Width
+        {
+            get
+            {
+                return this.htmlEditorControl.Width;
+            }
+
+            set
+            {
+                this.htmlEditorControl.Width = value;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// The add toolbar.
+        /// </summary>
+        public override void AddToolbar()
+        {
+            // Must exist because it exists at the base class
+        }
+
+        /// <summary>
+        /// The initialize.
+        /// </summary>
+        public override void Initialize()
+        {
+            this.htmlEditorControl = new Web.Textarea { ID = this.ControlID };
+        }
+    }
+}
